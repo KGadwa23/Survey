8d3f5bdbf004271d6e3a25298c4d4a8b764403ea
Tom Erickson <tom@tomerickson.me>
2016-09-12 15:48:26 -0500

    Now a working survey tool with comments

--- app/src/main/java/com/katrina/survey/SurveyActivity.java
+++ app/src/main/java/com/katrina/survey/SurveyActivity.java
@@ -1,3 +1,5 @@
+//by Katrina Gadwa
+
 package com.katrina.survey;
 
 import android.support.v7.app.AppCompatActivity;
@@ -8,12 +10,14 @@ import android.widget.TextView;
 
 public class SurveyActivity extends AppCompatActivity {
 
-    Button mChristmas;
+    Button mChristmas;              //List of all buttons and textview boxes
     Button mThanksgiving;
+    Button mResults;
+    Button mReset;
     TextView mChristmasOutcome;
     TextView mThanksgivingOutcome;
 
-    private int mChristmasStartOutcome = 0;
+    private int mChristmasStartOutcome = 0;     //starting value of zero for total counts
     private int mThanksgivingStartOutcome = 0;
 
     @Override
@@ -23,26 +27,39 @@ public class SurveyActivity extends AppCompatActivity {
 
         mChristmas = (Button) findViewById(R.id.christmasButton);
         mThanksgiving = (Button) findViewById(R.id.thanksgivingButton);
+        mResults = (Button) findViewById(R.id.resultsButton);
+        mReset = (Button) findViewById(R.id.resetButton);
         mChristmasOutcome = (TextView) findViewById(R.id.christmasOutcome);
         mThanksgivingOutcome = (TextView) findViewById(R.id.thanksgivingOutcome);
 
-        mChristmas.setOnClickListener(new View.OnClickListener() {
+        mChristmas.setOnClickListener(new View.OnClickListener() {      //when Christmas button is clicked, Christmas outcome is increased by 1
             @Override
             public void onClick (View v) {
-                mChristmasStartOutcome =+ 1;
+                mChristmasStartOutcome += 1;
+            }
+        });
+        mThanksgiving.setOnClickListener(new View.OnClickListener() {   //When THanksgiving button is clicked, Thanksgiving outcome is increased by 1
+            @Override
+            public void onClick (View v) {mThanksgivingStartOutcome += 1;
             }
         });
 
-        mThanksgiving.setOnClickListener(new View.OnClickListener() {
+        mResults.setOnClickListener(new View.OnClickListener() {    //when Results button is clicked, the accumulated Christmas and Thanksgiving outcomes are shown on the screen
             @Override
             public void onClick (View v) {
-                mThanksgivingStartOutcome += 1;
+                mChristmasOutcome.setText("Christmas: " + String.valueOf(mChristmasStartOutcome) + "        ");
+                mThanksgivingOutcome.setText("Thanksgiving: " + String.valueOf(mThanksgivingStartOutcome));
             }
         });
-    }
 
-    private void updateOutcomes() {
-        mChristmasOutcome.setText(mChristmasStartOutcome);
-        mThanksgivingOutcome.setText(mThanksgivingStartOutcome);
+        mReset.setOnClickListener(new View.OnClickListener() {      //When reset button is clicked, the accumulated Christmas and Thanksgiving outcomes return to zero and the new outcomes (zero) are shown on the screen
+            @Override
+            public void onClick (View v) {
+                mChristmasStartOutcome = 0;
+                mThanksgivingStartOutcome = 0;
+                mChristmasOutcome.setText("Christmas: " + String.valueOf(mChristmasStartOutcome) + "        ");
+                mThanksgivingOutcome.setText("Thanksgiving: " + String.valueOf(mThanksgivingStartOutcome));
+            }
+        });
     }
 }
